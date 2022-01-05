6b3746cf2fecf6b45245a86d69a34e2bd0c4dd3f
Daniel Chapman <danielgchap@hotmail.com>
2022-01-05 14:10:04 -0500

    Fixed network user fetch for edit profile

--- prayer_pals/lib/core/widgets/update_profile_pic.dart
+++ prayer_pals/lib/core/widgets/update_profile_pic.dart
@@ -1,12 +1,10 @@
 import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:prayer_pals/core/utils/constants.dart';
-import 'package:firebase_storage/firebase_storage.dart';
-import 'package:prayer_pals/features/user/repositories/storage_repo.dart';
-import 'package:prayer_pals/locator.dart';
 
-class UpdatePicture extends StatefulWidget {
+class UpdatePicture extends HookWidget {
   final Function(File) callback;
   const UpdatePicture({
     required BuildContext context,
@@ -14,11 +12,6 @@ class UpdatePicture extends StatefulWidget {
     Key? key,
   }) : super(key: key);
 
-  @override
-  _UpdatePictureState createState() => _UpdatePictureState();
-}
-
-class _UpdatePictureState extends State<UpdatePicture> {
   @override
   Widget build(BuildContext context) {
     return AlertDialog(
@@ -31,7 +24,7 @@ class _UpdatePictureState extends State<UpdatePicture> {
             Navigator.of(context).pop();
             var pickerFile =
                 await ImagePicker().pickImage(source: ImageSource.gallery);
-            if (pickerFile != null) widget.callback(File(pickerFile.path));
+            if (pickerFile != null) callback(File(pickerFile.path));
           },
           child: const Text(StringConstants.photos),
         ),
@@ -40,33 +33,11 @@ class _UpdatePictureState extends State<UpdatePicture> {
             Navigator.of(context).pop();
             var pickerFile =
                 await ImagePicker().pickImage(source: ImageSource.camera);
-            if (pickerFile != null) widget.callback(File(pickerFile.path));
+            if (pickerFile != null) callback(File(pickerFile.path));
           },
           child: const Text(StringConstants.camera),
         ),
       ],
     );
-  }
-
-  Future<void> _upload(String inputSource) async {
-    FirebaseStorage storage = FirebaseStorage.instance;
-    final picker = ImagePicker();
-    XFile? pickedImage;
-    try {
-      pickedImage = await picker.pickImage(
-          source: inputSource == 'camera'
-              ? ImageSource.camera
-              : ImageSource.gallery,
-          maxWidth: 60);
-      File imageFile = File(pickedImage!.path);
-
-      try {
-        await locator.get<StorageRepo>().uploadFile(imageFile);
-      } on FirebaseException catch (error) {
-        debugPrint(error.toString());
-      }
-    } catch (err) {
-      debugPrint(err.toString());
-    }
   }
 }
--- prayer_pals/lib/core/widgets/user_info_bar.dart
+++ prayer_pals/lib/core/widgets/user_info_bar.dart
@@ -4,6 +4,7 @@ import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:prayer_pals/core/providers/ppcuser_core_provider.dart';
 import 'package:prayer_pals/core/utils/constants.dart';
+import 'package:prayer_pals/features/user/models/ppcuser.dart';
 import 'package:prayer_pals/features/user/view/edit_profile.dart';
 import 'package:prayer_pals/core/utils/size_config.dart';
 import 'avatar_widget.dart';
@@ -18,54 +19,70 @@ class UserInfoBarWidget extends HookWidget {
 
   @override
   Widget build(BuildContext context) {
-    final ppcUser = useProvider(ppcUserCoreProvider).getCurrentUserModel();
-    return Container(
-      padding: const EdgeInsets.fromLTRB(0, 15, 0, 15),
-      child: Column(
-        children: [
-          Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Padding(
-                  padding: const EdgeInsets.fromLTRB(20, 0, 0, 0),
-                  child: PPCAvatar(
-                    radSize: 25,
-                    image: StringConstants.userIcon,
-                    networkImage: ppcUser!.imageURL,
-                  ),
-                ),
-                SizedBox(
-                  width: SizeConfig.safeBlockHorizontal! * 3,
-                ),
-                Expanded(
-                  flex: 7,
-                  child: Text(
-                    ppcUser.username,
-                    style: TextStyle(
-                      height: SizeConfig.safeBlockVertical! * .2,
-                      fontSize: SizeConfig.safeBlockVertical! * 3,
-                    ),
-                  ),
-                ),
-                Expanded(
-                  flex: 3,
-                  child: Visibility(
-                      visible: isSettings,
-                      child: InkWell(
-                          child: const Icon(Icons.edit),
-                          onTap: () {
-                            Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                    builder: (context) => EditProfilePage()));
-                          })),
-                )
-              ]),
-          const SizedBox(height: 10),
-          PPCstuff.divider,
-        ],
-      ),
-    );
+    return FutureBuilder(
+        future: useProvider(ppcUserCoreProvider).currentUserNetworkFetch(),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            PPCUser ppcUser = snapshot.data as PPCUser;
+
+            return Container(
+              padding: const EdgeInsets.fromLTRB(0, 15, 0, 15),
+              child: Column(
+                children: [
+                  Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.fromLTRB(20, 0, 0, 0),
+                          child: PPCAvatar(
+                            radSize: 25,
+                            image: StringConstants.userIcon,
+                            networkImage: ppcUser.imageURL,
+                          ),
+                        ),
+                        SizedBox(
+                          width: SizeConfig.safeBlockHorizontal! * 3,
+                        ),
+                        Expanded(
+                          flex: 7,
+                          child: Text(
+                            ppcUser.username,
+                            style: TextStyle(
+                              height: SizeConfig.safeBlockVertical! * .2,
+                              fontSize: SizeConfig.safeBlockVertical! * 3,
+                            ),
+                          ),
+                        ),
+                        Expanded(
+                          flex: 3,
+                          child: Visibility(
+                              visible: isSettings,
+                              child: InkWell(
+                                  child: const Icon(Icons.edit),
+                                  onTap: () {
+                                    Navigator.push(
+                                        context,
+                                        MaterialPageRoute(
+                                            builder: (context) =>
+                                                EditProfilePage()));
+                                  })),
+                        )
+                      ]),
+                  const SizedBox(height: 10),
+                  PPCstuff.divider,
+                ],
+              ),
+            );
+          } else {
+            return const Center(
+              child: SizedBox(
+                width: 40,
+                height: 40,
+                child: CircularProgressIndicator(),
+              ),
+            );
+          }
+        });
   }
 }

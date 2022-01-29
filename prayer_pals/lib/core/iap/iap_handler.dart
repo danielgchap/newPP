f162444f17f0ffda92a6232f851ee7e5242bae69
Daniel Chapman <danielgchap@hotmail.com>
2022-01-28 20:03:24 -0500

    Working on IAPs

---
+++ prayer_pals/lib/core/iap/iap_handler.dart
@@ -0,0 +1,125 @@
+import 'dart:async';
+import 'dart:io';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:in_app_purchase/in_app_purchase.dart';
+import 'package:in_app_purchase_android/billing_client_wrappers.dart';
+import 'package:in_app_purchase_android/in_app_purchase_android.dart';
+import 'package:in_app_purchase_storekit/in_app_purchase_storekit.dart';
+import 'package:in_app_purchase_storekit/store_kit_wrappers.dart';
+import 'package:prayer_pals/core/iap/consumable_store.dart';
+
+final groupPrayerControllerProvider =
+    Provider<IAPHandler>((ref) => IAPHandler(ref.read));
+
+class IAPHandler {
+  final String _kRemovedAdsId = 'com.prayerpals.removeads';
+  final String _kStartGroupId = 'com.prayerpals.startgroup';
+  final Reader read;
+
+  final InAppPurchase _inAppPurchase = InAppPurchase.instance;
+  late StreamSubscription<List<PurchaseDetails>> _subscription;
+  List<String> _notFoundIds = [];
+  List<ProductDetails> _products = [];
+  List<PurchaseDetails> _purchases = [];
+  List<String> _consumables = [];
+  bool _isAvailable = false;
+  bool _purchasePending = false;
+  bool _loading = true;
+  String? _queryProductError;
+
+  List<String>? _kProductIds;
+
+  IAPHandler(this.read) {
+    _kProductIds = <String>[
+      _kRemovedAdsId,
+      _kStartGroupId,
+    ];
+    _init();
+  }
+
+  _init() {
+    final Stream<List<PurchaseDetails>> purchaseUpdated =
+        _inAppPurchase.purchaseStream;
+    _subscription = purchaseUpdated.listen((purchaseDetailsList) {
+      _listenToPurchaseUpdated(purchaseDetailsList);
+    }, onDone: () {
+      _subscription.cancel();
+    }, onError: (error) {
+      // handle error here.
+    });
+    _initStoreInfo();
+  }
+
+  Future<void> _initStoreInfo() async {
+    final bool isAvailable = await _inAppPurchase.isAvailable();
+    if (!isAvailable) {
+      _isAvailable = isAvailable;
+      _products = [];
+      _purchases = [];
+      _notFoundIds = [];
+      _consumables = [];
+      _purchasePending = false;
+      _loading = false;
+
+      return;
+    }
+
+    if (Platform.isIOS) {
+      var iosPlatformAddition = _inAppPurchase
+          .getPlatformAddition<InAppPurchaseStoreKitPlatformAddition>();
+      await iosPlatformAddition.setDelegate(ExamplePaymentQueueDelegate());
+    }
+
+    ProductDetailsResponse productDetailResponse =
+        await _inAppPurchase.queryProductDetails(_kProductIds!.toSet());
+    if (productDetailResponse.error != null) {
+      _queryProductError = productDetailResponse.error!.message;
+      _isAvailable = isAvailable;
+      _products = productDetailResponse.productDetails;
+      _purchases = [];
+      _notFoundIds = productDetailResponse.notFoundIDs;
+      _consumables = [];
+      _purchasePending = false;
+      _loading = false;
+      return;
+    }
+
+    if (productDetailResponse.productDetails.isEmpty) {
+      _queryProductError = null;
+      _isAvailable = isAvailable;
+      _products = productDetailResponse.productDetails;
+      _purchases = [];
+      _notFoundIds = productDetailResponse.notFoundIDs;
+      _consumables = [];
+      _purchasePending = false;
+      _loading = false;
+      return;
+    }
+
+    List<String> consumables = await ConsumableStore.load();
+    _isAvailable = isAvailable;
+    _products = productDetailResponse.productDetails;
+    _notFoundIds = productDetailResponse.notFoundIDs;
+    _consumables = consumables;
+    _purchasePending = false;
+    _loading = false;
+  }
+}
+
+/// Example implementation of the
+/// [`SKPaymentQueueDelegate`](https://developer.apple.com/documentation/storekit/skpaymentqueuedelegate?language=objc).
+///
+/// The payment queue delegate can be implementated to provide information
+/// needed to complete transactions.
+class ExamplePaymentQueueDelegate implements SKPaymentQueueDelegateWrapper {
+  @override
+  bool shouldContinueTransaction(
+      SKPaymentTransactionWrapper transaction, SKStorefrontWrapper storefront) {
+    return true;
+  }
+
+  @override
+  bool shouldShowPriceConsent() {
+    return false;
+  }
+}

0d711ff7386fe36f3db562aee9da7cdc10c1fdc4
Daniel Chapman <danielgchap@hotmail.com>
2022-02-11 21:28:01 -0500

    Got push notifications working on Android

--- prayer_pals/lib/main.dart
+++ prayer_pals/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:google_mobile_ads/google_mobile_ads.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
@@ -6,11 +7,16 @@ import 'package:overlay_support/overlay_support.dart';
 import 'package:prayer_pals/core/services/notification_service.dart';
 import 'prayer_pals_app.dart';
 
+Future _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  debugPrint("Handling a background message: ${message.messageId}");
+}
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   MobileAds.instance.initialize();
   await NotificationService().init(); //
   await Firebase.initializeApp();
+  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
 
   runApp(
     const ProviderScope(
